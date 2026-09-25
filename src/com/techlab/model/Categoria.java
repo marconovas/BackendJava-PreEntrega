6700b166e70065d6fa738182383639992846d967
Marco Novas <marco.nicolas.novas@gmail.com>
2026-09-24 23:55:03 -0300

    first commit

---
+++ src/com/techlab/model/Categoria.java
@@ -0,0 +1,40 @@
+package com.techlab.model;
+
+public class Categoria {
+    private int codigo;
+    private String nombre;
+    private String descripcion;
+
+    //CONSTRUCTOR
+    public Categoria(int codigo, String nombre, String descripcion) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    //GETTERS
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    //SETTERS
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
